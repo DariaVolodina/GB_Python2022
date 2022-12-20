a7fe98d08f741cdae13d4e386547d1abfe77154c
Daria <fedu@inbox.ru>
2022-12-20 13:43:08 +0300

    HW 8 done

--- Homework08/task01.py
+++ Homework08/task01.py
@@ -3,3 +3,29 @@
 # В этом классе реализуйте метод show_my_soup(),
 # выводящий на печать «Суп - {Основной продукт}» в случае наличия добавки,
 # а иначе отобразится следующая фраза: «Обычный кипяток»
+
+class Soup():
+    def __init__(self, main_product):
+        self.main_product = main_product
+
+    def show_my_soup(self):
+        if self.main_product.isalpha():
+           print(f'Суп - {self.main_product}')
+        else:
+            print('Обычный кипяток')
+
+
+soup_1 = Soup('свекла')
+soup_2 = Soup('рыба')
+soup_3 = Soup('капуста')
+soup_4 = Soup('курица')
+soup_5 = Soup('')
+my_soup = Soup(' ')
+
+soup_1.show_my_soup()
+soup_2.show_my_soup()
+soup_3.show_my_soup()
+soup_4.show_my_soup()
+soup_5.show_my_soup()
+my_soup.show_my_soup()
+
--- Homework08/task02.py
+++ Homework08/task02.py
@@ -1,6 +1,43 @@
 # Делаем программу , где используем классы.
 # Где класс будет содержать имя студента name,
 # номер группы group и список полученных оценок progress.
+
 # В самой программе вводим список студентов.
 # После выводим список , отсортированный по имени,
 # потом выводятся студенты, имеющие неудовлетворительные оценки.
+
+from random import choice
+from random import randint
+
+class Student():
+    def __init__(self, name, group_num, progress):
+        self.name = name
+        self.group_num = group_num
+        self.progress = progress
+        # print('done')
+
+    def show_name(self):
+        return self.name
+
+
+students_list = ['Ivanov', 'Stepanov', 'Sergeev', 'Matveev', 'Petrov', 'Semyonov', 'Ryabikov', 'Nesterov', 'Adams']
+students_list.sort()
+gr = ['group A', 'group B', 'group C']
+scores = [2, 3, 3, 4, 4, 4, 5, 5, 5, 5] # иначе, по теории вероятности, двоичников будет слишком много
+neud_list = []
+
+for i in range(len(students_list)):
+    x = 'st' + str(i + 1)
+    score1 = []
+    num_of_scores = randint(5, 10)
+    for j in range(num_of_scores):
+        score1.append(choice(scores))
+    if 2 in score1:
+        neud_list.append(students_list[i])
+    # print(students_list[i])
+    # print(x)
+    # print(score1)
+    x = Student(students_list[i], choice(gr), score1)
+
+print(students_list)
+print('Неудотворительные оценки имеют следующие студенты: \n', '\n '.join(neud_list))
